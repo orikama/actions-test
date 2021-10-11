6fb2e732c3dd007393da437532a910d90d5acd52
Vladimir Matushkin <matushkin.vladimir98@gmail.com>
2021-10-11 17:51:08 +0500

    10

--- env.py
+++ env.py
@@ -1,3 +1,3 @@
 import os
 
-print(f"PREVIOUS_SHA: {os.environ['PREVIOUS_SHA']} \nCURRENT_SHA: {os.environ['nCURRENT_SHA']}")
+print(f"PREVIOUS_SHA: {os.environ['PREVIOUS_SHA']}\nCURRENT_SHA: {os.environ['CURRENT_SHA']}")
